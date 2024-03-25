f191c3f8a990a7326a88de5d51cf31a8041caf55
Andrey <zizkun.av@list.ru>
2024-03-25 22:17:23 +0400

    Тест

--- src/test/java/dragAndDrope/DragAndDrop.java
+++ src/test/java/dragAndDrope/DragAndDrop.java
@@ -1,13 +1,15 @@
 package dragAndDrope;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.DragAndDropOptions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.interactions.Actions;
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class DragAndDrop {
+
 
     @BeforeAll
     static void beforeAll(){
@@ -18,9 +20,30 @@ public class DragAndDrop {
     }
 
     @Test
-    void doDragAndDrop() {
+    void doDragAndDropOne() {
 
         open("/drag_and_drop");
 
+        $("#column-a").dragAndDrop(DragAndDropOptions.to("#column-b"));
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+    }
+    @Test
+    void doDragAndDropTwo() {
+
+        open("/drag_and_drop");
+
+        actions().dragAndDrop($("#column-a"), $("#column-b")).perform();
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+}
+    @Test
+    void doDragAndDropThree() {
+
+        open("/drag_and_drop");
+
+        actions().moveToElement($("#column-a")).clickAndHold().moveToElement($("#column-b")).release().perform();
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
 }
 }
